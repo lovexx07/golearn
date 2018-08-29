9bce941577266cbc7dc7ef91d7d1138cd7c5c75f
lovexx07 <lovexx07@outlook.com>
2018-08-30 07:12:46 +0800

    Gopath    map 添加数据 删除key

---
+++ src/golearn/test9/q1/demo18.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//var badMap = map[interface{}]int{
+	//	"1":1,
+	//	[]int{2} : 2, //报错
+	//	3: 3,
+	//}
+
+	//var badMap map[[1][]string]int
+
+	//type Badkey1 struct {
+	//	slice []string
+	//}
+	//var badMap map[Badkey1]int
+
+
+
+	fmt.Println(badMap)
+}
