cbc4573cc0bc7de08ae794b04c6a60b7cd4f21c1
lovexx07 <lovexx07@outlook.com>
2018-09-24 07:00:02 +0800

    Gopath    errors.New("...") 创建一个错误对象

---
+++ src/golearn/test18/q0/demo44.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func echo(request string) (response string, err error) {
+	if request == ""{
+		err = errors.New("empty content")
+		return
+	}
+	response = fmt.Sprintf("echo: %s", request)
+	return
+}
+
+func main() {
+	for _, req := range []string{"", "hello!"}{
+		fmt.Printf("request: %s\n", req)
+		resp, err := echo(req)
+		if err != nil{
+			fmt.Printf("error: %s \n", err)
+			continue
+		}
+		fmt.Printf("response: %s\n", resp)
+	}
+
+	fmt.Println()
+
+	err1 := fmt.Errorf("invalid content: %s", "#$%")
+	err2 := errors.New(fmt.Sprintf("invalid content: %s", "#$%"))
+	if err1.Error() == err2.Error(){
+		fmt.Println("The error message in err1 and err2 are the same")
+	}
+
+
+}
